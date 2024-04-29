ef4a0831020662d295a1bd6fd455264c46754668
BASİL AVAD <69681817+BasilAvad@users.noreply.github.com>
2024-04-29 21:45:29 +0300

    Test the git push func in this project this commit will be removed

--- Examples/esp32/PlatformIO IDE/src/main.cpp
+++ Examples/esp32/PlatformIO IDE/src/main.cpp
@@ -12,8 +12,8 @@ void setup()
 void loop()
 {
   int sensorValue1 = analogRead(35);
-  int sensorValue2 = analogRead(32);
-  float adc1 = sensorMov.mafunc(sensorValue1);
+  int sensorValue2 = analogRead(32); 
+  float adc1 = sensorMov.mafunc(sensorValue1); // Overloading the Function 
   float adc2 = sensorMov2.mafunc(sensorValue2);
   Serial.print("Movingaverage1:");
   Serial.println(adc1);
